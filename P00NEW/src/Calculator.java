db499905db1cd3e3ebf0986f0a4efbcc6e25c785
travi <travi@172.29.82.2>
2022-05-27 10:17:58 +0800

    first push to P00NEW respository in Github

---
+++ P00NEW/src/Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Bobo Chan
+ * Student ID: 12345
+ * Class: W68X
+ * Date/Time created: Friday 27-05-2022 09:55
+ */
+
+/**
+ * @author travi
+ *
+ */
+public class Calculator {
+	
+	public int add (int a, int b) {
+		 return a + b;
+		}
+	public int substract (int a, int b) {
+		 return a - b;
+		}
+}
